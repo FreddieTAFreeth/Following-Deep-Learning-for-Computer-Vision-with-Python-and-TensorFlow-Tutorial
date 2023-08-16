fe8759f9967964e74c7ff3cb399e3286c2072ee1
Frederick (Fred) T. A. Freeth <114069006+FreddieTAFreeth@users.noreply.github.com>
2023-08-16 13:07:21 +0100

    Update Car_Price_Prediction.py

--- Car_Price_Prediction.py
+++ Car_Price_Prediction.py
@@ -4,6 +4,23 @@
 # ============================================================================ #
 # Following https://www.youtube.com/watch?v=IA3WxTTPXqQ.
 
+if __name__ == "__main__":
+    # The Task:
+    # ---------
+    # We want to predict the price of second-hand cars given several parameters.
+    # Owners of these cars will specify:
+    # - "years": how old the car is,
+    # - "km": how many kilometres the car has driven,
+    # - "rating": the rating of the vehicle,
+    # - "condition": the physical condition of the vehicle,
+    # - "economy": the fuel economy of the car,
+    # - "top speed": the maximum speed the car can drive,
+    # - "hp" the horse-power of the car's engine,
+    # - "torque": the engine torque,
+    # - "current price": the current price of the car
+
+    
+    
 
 # ============================================================================ #
 # Car PRice Prediction - Code End                                              |
