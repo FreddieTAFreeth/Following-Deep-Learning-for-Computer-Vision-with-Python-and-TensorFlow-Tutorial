15a7d70b291dd6d2614bf45f52dab767a1c49079
Frederick (Fred) T. A. Freeth <114069006+FreddieTAFreeth@users.noreply.github.com>
2023-08-24 15:53:09 +0100

    Added task background.

--- Malaria_Diagnosis.py
+++ Malaria_Diagnosis.py
@@ -4,6 +4,28 @@
 # ============================================================================ #
 # Following https://www.youtube.com/watch?v=IA3WxTTPXqQ.
 
+import csv
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    # The Task:
+    # ---------------------------
+    # In 2021, almost half of the World's population was at risk of malaria. In
+    # that year there were an aestimated 247 million cases of malaria worldwide.
+    # The WHO (World Health Organisation) African Region experiences a massive
+    # disproportion compared to the rest of the world. In 2021, the Region is
+    # home to 95% of malaria cases and 96% of malaria deaths, of which there
+    # 619,000. Also, children under 5 accounted for approximately 80% of all
+    # deaths in the Region. Source (accessed 24/08/2023): 
+    # https://www.who.int/news-room/fact-sheets/detail/malaria
+
+    
+    
+    
 # ============================================================================ #
 # Malaria Diagnosis - Code End                                                 |
 # ============================================================================ #
