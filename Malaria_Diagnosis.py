2105c2c2d7dc844c62ff8a767e22e2c3389b61d4
Frederick (Fred) T. A. Freeth <114069006+FreddieTAFreeth@users.noreply.github.com>
2023-08-25 13:57:37 +0100

    Update Malaria_Diagnosis.py

--- Malaria_Diagnosis.py
+++ Malaria_Diagnosis.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # ============================================================================ #
-# Malaria Diagnosis - Frederick T. A. Freeth                        24/08/2023 |
+# Malaria Diagnosis - Frederick T. A. Freeth                        25/08/2023 |
 # ============================================================================ #
 # Following https://www.youtube.com/watch?v=IA3WxTTPXqQ.
 
@@ -22,18 +22,28 @@ if __name__ == "__main__":
     # 619,000. Also, children under 5 accounted for approximately 80% of all
     # deaths in the Region. Source (accessed 24/08/2023): 
     # https://www.who.int/news-room/fact-sheets/detail/malaria
-
+    #
     # In this file, we will build a machine learning model based on convolutional
     # neural networks (CNNs) that aim to diagnose malaria. The data we will use
-    # are microscopy photos of cells. Our model takes in as input a photo of a
-    # cell, and as output gives a binary classification of whether the person
-    # is infected with malaria or not:
+    # are microscopy photos of cells. These cells are gathered via puncture of a
+    # fingertip, and blood is smeared out on a slide. We can have two types of
+    # smears: thin or thick. Our dataset is concerned with only thin smears since
+    # these are used in the differentiaton of leukocytes. A thick blood smear is
+    # used for the diagnosis of blood protozoan parasites and blood abnormalities.
+    # Each slide photograph will contain many cells. We will need to analyse each
+    # cell seperately by extracting it from the image of th entire slide.
+    #
+    # Our model takes in as input
+    # a photo of the cells on the slide, and as output gives a binary decision of
+    # whether the person is infected with malaria or not:
     #
     #     Inputs                         Outputs
     #     ‾‾‾‾‾‾                         ‾‾‾‾‾‾‾
     #   [ Cell   ]     [       ]     [ Host Status: ]
     #   [ Photo- ] --> [ Model ] --> [ infected or  ]
     #   [ graph  ]     [       ]     [ not infected ]
+    #
+    # Image data is represented by a 2d array with an RGB colour space.
     
     
     
